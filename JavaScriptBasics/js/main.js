2d89d4d21b084284cef07c5db2d593077de812d6
kubore <63027012+kubore@users.noreply.github.com>
2020-04-02 11:06:32 +0900

    initialCommit

---
+++ JavaScriptBasics/js/main.js
@@ -0,0 +1,41 @@
+'use strict'; //厳密なエラーチェックをしやすくなる
+
+const score = 85;
+
+if (score >= 80){
+  console.log('Greate');
+
+}
+
+
+
+// console.log(price > 1000);
+
+
+// console.log(parseInt('5', 10) + 3);
+
+// console.log(parseInt('hello', 10) + 3); //NaN:Not a Number
+
+// console.log(typeof 'hello');
+// console.log(typeof 5);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+
+// let price = 500;
+// price = price +100;
+// price += 100;
+// price *= 2; //price = price *2
+
+
+// console.log('it\'s me!');
+// console.log('it\'s \tme!');
+// console.log('it\'s \tme!');
+
+
+// 定数 const
+// 変数 let varは古くて色々問題がある→最近はlet
+// var price = 150; //varとの違いは？？→ varは再代入可能
+// let price = 150;
